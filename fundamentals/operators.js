d00be1cf4916c7b36d6829c125ebf2dc57ef4448
Raffian-moin <raffianmoin69@gmail.com>
2022-02-19 23:17:45 +0600

    operators:increment and decrement

---
+++ fundamentals/operators.js
@@ -0,0 +1,12 @@
+// The prefix form returns the new value while the postfix form 
+// returns the old value (prior to increment/decrement).
+
+let age = 1;
+
+console.log(`age: ${age++}`);
+console.log(`age: ${age++}`);
+
+let marks = 1;
+
+console.log(`marks: ${marks++}`);
+console.log(`marks: ${marks++}`);
