e32123dc9ccc2a1b8edd2967c43b78cf219497cc
Raffian-moin <raffianmoin69@gmail.com>
2022-02-19 22:47:32 +0600

    data types

---
+++ fundamentals/dataTypes/number.js
@@ -0,0 +1,4 @@
+let name = 'ridwan';
+
+let message = `${name} is awesome`;
+console.log(name, message);
---
+++ fundamentals/dataTypes/undefined-null.js
@@ -0,0 +1,7 @@
+let myName = null;
+
+let message
+
+console.log(myName,"\n",message);
+console.log(typeof myName);
+console.log(typeof message);
