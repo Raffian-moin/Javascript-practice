c0f8a3f686466281d197fbe8001e4c492fdcb7d1
Raffian-moin <raffianmoin69@gmail.com>
2022-02-28 01:17:26 +0600

    set

--- data-types/map-set.js
+++ data-types/map-set.js
@@ -19,3 +19,32 @@ let flowers = new Map();
 flowers.set('1', "tulip").set('2', 'rose');
 
 console.log(flowers);
+
+let recipeMap = new Map([
+    ['cucumber',100],
+    ['apple',100],
+]);
+
+for (let vegetable of recipeMap.keys()) {
+    console.log(vegetable);
+}
+
+for (let amount of recipeMap.values()) {
+  console.log(amount);
+}
+
+for (let entry of recipeMap) {
+  console.log(`there are ${entry[1]} ${entry[0]} in my kitchen`);
+}
+
+let set = new Set();
+
+console.log(set.add('moin'));
+console.log(set.add('rahman'));
+console.log(set.add('rahman'));
+console.log(set);
+
+for (let name of set) {
+    console.log(name);
+}
+// console.log(set.values());
