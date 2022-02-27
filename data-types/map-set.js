7bb00bca9767248fc6be706f70666af5b5766251
Raffian-moin <raffianmoin69@gmail.com>
2022-02-27 23:51:25 +0600

    map

---
+++ data-types/map-set.js
@@ -0,0 +1,21 @@
+let map = new Map();
+
+map.set('1',"moin");
+
+console.log(map.get('1'));
+
+let name = {
+    1: 'moin'
+}
+
+let nameMap = new Map();
+
+nameMap.set('name-key',name);
+
+console.log(nameMap.get('name-key'));
+
+let flowers = new Map();
+
+flowers.set('1', "tulip").set('2', 'rose');
+
+console.log(flowers);
