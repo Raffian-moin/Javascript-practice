652c50b8a388236f608516e1e5630f645d403876
Raffian-moin <raffianmoin69@gmail.com>
2022-10-03 13:46:39 +0600

    variable revisited

---
+++ Javascript Revisited/variable.js
@@ -0,0 +1,24 @@
+let name = "moin";
+let age = 26;
+
+console.log(name, age);
+
+// Comma separated multiple variables
+let status = "rich",
+    intelligence = true;
+
+console.log(status, intelligence);
+
+// constant
+const pi = 3.1416;
+console.log(pi);
+
+/*
+    [constant name in UPPERCASE]
+
+    There is a widespread practice to use constants as aliases 
+    for difficult-to-remember values that are known prior to execution
+*/
+const COLOR_RED = "#F00";
+
+console.log(COLOR_RED);
