11f6555081a96930b729ebc24fa63cc67f58e68c
Raffian-moin <raffianmoin69@gmail.com>
2022-10-04 10:56:43 +0600

    data type basics revisited

---
+++ Javascript Revisited/data_type_basics.js
@@ -0,0 +1,10 @@
+let age = 10;
+let name = `moin, age ${age}`
+let isEducated = true;
+
+console.log(age, name, isEducated);
+
+// typeof returns the type of the variable
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isEducated);
