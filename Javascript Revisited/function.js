36965476d9373ef0e538d5fa840ff86183f56f16
Raffian-moin <raffianmoin69@gmail.com>
2022-10-05 18:57:16 +0600

    function basics revisited

---
+++ Javascript Revisited/function.js
@@ -0,0 +1,13 @@
+function sayHello(){
+    console.log("Hi");
+}
+
+sayHello();
+
+// function can be called before declaration
+
+sayGoodbye();
+
+function sayGoodbye(){
+    console.log("Bye");
+}
