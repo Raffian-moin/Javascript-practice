3a7fe1845d017c02bd0247fa45ecd192a5a2feb5
Raffian-moin <raffianmoin69@gmail.com>
2023-08-28 11:31:33 +0600

    this property in arrow function

--- Javascript/object/this property/this_in_arrow_function.js
+++ Javascript/object/this property/this_in_arrow_function.js
@@ -1,11 +1,33 @@
+/*
+arrow function has no this:
+---------------------------
+arrow function has no this function bind with it.
+arrow function use this from its outer normal function.
+
+*/
+
+// it will cause error because the outer normal function can't reach this.name
+// as there is no name property in the object
 let user = {
-    name: 'moin',
-    ok () {
-        return hello = () => {
-          return "Hello World!" + this.name;
+    greet () {
+        return sayHello = () => {
+          return "Hello " + this.name;
         };
     } 
-    
 }
-let x = user.ok();
-console.log(x());
+
+// it will work fine
+let greet1 = user.greet()
+console.log(greet1())
+
+let admin = {
+  name: 'moin',
+  greet() {
+    return (sayHello = () => {
+      return "Hello " + this.name;
+    });
+  },
+};
+
+let greet2 = admin.greet();
+console.log(greet2());
