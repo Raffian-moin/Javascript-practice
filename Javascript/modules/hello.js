8bf3c0bfae4669083ecbf8c0cf974868db98885f
Raffian-moin <raffianmoin69@gmail.com>
2023-12-13 23:28:06 +0600

    module: import and export basics

---
+++ Javascript/modules/hello.js
@@ -0,0 +1,5 @@
+// we import modules that have a default name without curly brace
+// here we imported greet without curly brace because it was exported with default name
+import greet from "./main.js";
+
+console.log(greet());
---
+++ Javascript/modules/main.js
@@ -0,0 +1,5 @@
+// we can export a file with default keyword
+// usually we export one thing/class/method from a file
+export default function greet() {
+    return "hello, Moin";
+}
